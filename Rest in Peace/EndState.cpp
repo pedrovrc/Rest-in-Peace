fd503bd5bcbb784413cc097a1e44cbd17a61cbd4
Pedro Victor <pedrovrc32@gmail.com>
2023-12-12 00:09:53 -0300

    Mudanças em EndState
    Cena pós-combate adicionada.
    Música de derrota adicionada.

--- Rest in Peace/EndState.cpp
+++ Rest in Peace/EndState.cpp
@@ -5,17 +5,22 @@
 #include "Text.h"
 #include "GameData.h"
 #include "GeneralFunctions.h"
+#include "ScrollerText.h"
 
 EndState::EndState(string opponent, string ambient) {
 	quitRequested = false;
 	this->opponent = opponent;
 	this->ambient = ambient;
-	flagInitialScreen = false;
+	flagInitialScreen = true;
 	flagChoice = false;
+	end = false;
 }
 
 EndState::~EndState() {
-
+	while (objectArray.empty() == false) {
+		objectArray.erase(objectArray.end() - 1);
+	}
+	objectArray.clear();
 }
 
 void EndState::LoadAssets() {
@@ -24,26 +29,33 @@ void EndState::LoadAssets() {
 	LoadTextAndMusic();
 
 	LoadOpponent(opponent);
-
-	flagInitialScreen = true;
 }
 
 void EndState::LoadAmbient(string type) {
 	GameObject* go_bg = new GameObject();
 	string filename;
 
-	if (type == "living room") {
-		filename = "img/old_haunted_house_full_blur.png";
+	if (flagInitialScreen) {
+		if (type == "living room") {
+			filename = "img/old_haunted_house_full_blur.png";
+		}
+		CreateAddSprite(go_bg, filename, 1, 0, *new Vec2(0,0), WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 
-	CreateAddSprite(go_bg, filename, 1, 0, *new Vec2(0,0), WINDOW_WIDTH, WINDOW_HEIGHT);
+	if (flagChoice) {
+		if (type == "living room") {
+			filename = "img/old_haunted_house_blur.png";
+		}
+		CreateAddSprite(go_bg, filename, 1, 0, *new Vec2(0,0), 550, 900);
+	}
 	AddObject(go_bg);
 }
 
 void EndState::LoadTextAndMusic() {
+	Game& game = game.GetInstance();
 	Colors color = Colors::GetInstance();
-	GameObject* textQuit = new GameObject();
 
+	GameObject* textQuit = new GameObject;
 	if (GameData::playerVictory) {
 		// vitoria
 		// carrega bg
@@ -59,37 +71,294 @@ void EndState::LoadTextAndMusic() {
 		// carrega musica
 		backgroundMusic.Open("audio/loseMusic.mp3");
 	}
-
 	textQuit->box.SetCenterPosition(*new Vec2(800,450));
 	AddObject(textQuit);
 
 	GameObject* textPrompt = new GameObject;
-	CreateAddText(textPrompt, NK57, 50, "Pressione espaço para continuar", -1, -1, color.white, 0);
-	textPrompt->box.SetCenterPosition(*new Vec2(800,550));
+	if (game.gameData.playerVictory) {
+		CreateAddText(textPrompt, NK57, 50, "Pressione espaço para continuar", -1, -1, color.white, 0);
+	} else {
+		CreateAddText(textPrompt, NK57, 50, "Pressione espaço para recomeçar", -1, -1, color.white, 0);
+	}
+	textPrompt->box.SetCenterPosition(*new Vec2(800,800));
 	AddObject(textPrompt);
 }
 
 void EndState::LoadOpponent(string type) {
-	if (type == "diabrete") {
-		// carrega sprite do diabrete dependendo de vitória ou derrota
+	Game& game = game.GetInstance();
+	Colors& color = color.GetInstance();
 
-		// carrega texto do diabrete
-		// "Você não resistiu aos truques dos diabretes."
+	if (flagInitialScreen) {
+		GameObject* text = new GameObject;
+		if (type == "diabrete") {
+			// carrega texto do diabrete
+			if (game.gameData.playerVictory) {
+				CreateAddText(text, NK57, 30, "Você derrotou o diabrete!", -1, -1, color.white, 0);
+			} else {
+				CreateAddText(text, NK57, 30, "Você não resistiu aos truques do diabrete.", -1, -1, color.white, 0);
+			}
+		}
+		text->box.SetCenterPosition(*new Vec2(800, 600));
+		AddObject(text);
+	}
+
+	if (flagChoice) {
+		GameObject* opponent = new GameObject;
+		GameObject* name = new GameObject;
+		if (type == "diabrete") {
+			CreateAddCenteredSprite(opponent, "img/enemies/diabrete.png", 1, 0, *new Vec2(275, 450), -1, -1);
+			CreateAddText(name, NK57, 30, "Diabrete", -1, -1, color.white, 0);
+			name->box.SetCenterPosition(*new Vec2(275,40));
+		}
+		AddObject(opponent);
+		AddObject(name);
 	}
 }
 
 void EndState::Update(float dt) {
+	Game& game = game.GetInstance();
 	InputManager* input = &(InputManager::GetInstance());
 	UpdateArray(dt);
 
-	// checa por jogar novamente
-	if (input->KeyPress(SPACE_KEY)) {
-		popRequested = true;
+	if (end) {
+		Button* button = GetButton("continue");
+		if (button->IsHovered() && input->MousePress(LEFT_MOUSE_BUTTON)) {
+			popRequested = true;
+		}
+	}
+
+	if (flagChoice) {
+		Button* option1 = GetButton("deal");
+		Button* option2 = GetButton("exorcism");
+
+		if (option1->IsHovered() && input->MousePress(LEFT_MOUSE_BUTTON)) {
+			// pacto
+			DeleteText("vitoria diabrete");
+			DeleteButton("deal");
+			DeleteButton("exorcism");
+			LoadButton("continue", "centered");
+			LoadText("pacto diabrete");
+			end = true;
+			flagChoice = false;
+		}
+
+		if (option2->IsHovered() && input->MousePress(LEFT_MOUSE_BUTTON)) {
+			// exorcismo
+			DeleteText("vitoria diabrete");
+			DeleteButton("deal");
+			DeleteButton("exorcism");
+			LoadButton("continue", "centered");
+			LoadText("exorcizar diabrete");
+			end = true;
+			flagChoice = false;
+		}
+	}
+
+	// checa por espaço
+	if (flagInitialScreen && input->KeyPress(SPACE_KEY)) {
+		if (game.gameData.playerVictory) {
+			// continuar -> cena de pacto
+			flagChoice = true;
+			flagInitialScreen = false;
+
+			EraseAllObjects();
+			LoadScreen();
+			LoadAmbient(ambient);
+			LoadOpponent(opponent);
+			LoadText("vitoria diabrete");
+			LoadButton("deal", "1 of 2");
+			LoadButton("exorcism", "2 of 2");
+			LoadPlayerProfile();
+		} else {
+			// recomeçar -> reiniciar jogo
+			popRequested = true;
+		}
 	}
 
 	// checa por quit
 	if (input->QuitRequested() || input->KeyPress(ESCAPE_KEY)) {
 		quitRequested = true;
+	}
+}
+
+void EndState::LoadPlayerProfile() {
+	Colors& color = Colors::GetInstance();
+
+	// carrega avatar do player
+	GameObject* avatar = new GameObject;
+	CreateAddSprite(avatar, "img/MC/avatar v2.png", 1, 0,
+					*new Vec2(AVATAR_POS_X, AVATAR_POS_Y), -1, -1);
+	AddObject(avatar);
+
+	// carrega textos
+	// carrega nome
+	GameObject* name = new GameObject;
+	CreateAddText(name, "nk57-monospace-no-rg.otf", 40, "Sofia", -1, -1, color.white, 0);
+	name->box.SetCenterPosition(*new Vec2(1505, 40));
+	AddObject(name);
+
+	Player* player = Player::GetInstance();
+
+	// Carrega HUD HP -------------------
+	// barra vermelha
+	GameObject* redbar = new GameObject;
+	CreateAddSprite(redbar, "img/resources/red bar.png", 1, 0,
+					*new Vec2(HUD_POS_X, HUD_POS_Y), HUDBAR_W, HUDBAR_H);
+	AddObject(redbar);
+
+	// barra verde
+	GameObject* hpbar = new GameObject;
+	CreateAddSprite(hpbar, "img/resources/green bar.png", 1, 0,
+					*new Vec2(HUD_POS_X, HUD_POS_Y),
+					HUDBAR_W * ((float)player->GetHP()/PLAYER_MAX_HP), HUDBAR_H);
+	AddObject(hpbar);
+
+	// string na barra
+	string hp = "Vida: " + to_string(player->GetHP()) + "/" + to_string(PLAYER_MAX_HP);
+	GameObject* go_hpdata = new GameObject();
+	CreateAddText(go_hpdata, NK57, 15, hp, -1, -1, color.white, 0);
+	go_hpdata->box.SetCenterPosition(redbar->box.GetCenter());
+	AddObject(go_hpdata);
+
+	// Carrega HUD SP -----------------------
+	// barra cinza.
+	GameObject* graybar = new GameObject;
+	CreateAddSprite(graybar, "img/resources/gray bar.png", 1, 0,
+			*new Vec2(HUD_POS_X, HUD_POS_Y + HUD_Y_OFFSET), HUDBAR_W, HUDBAR_H);
+	AddObject(graybar);
+
+	// barra branca
+	GameObject* spbar = new GameObject;
+	CreateAddSprite(spbar, "img/resources/white bar.png", 1, 0,
+				*new Vec2(HUD_POS_X, HUD_POS_Y + HUD_Y_OFFSET),
+				HUDBAR_W * ((float)player->GetSP()/PLAYER_MAX_SP), HUDBAR_H);
+	AddObject(spbar);
+
+	// string na barra
+	string sp = "Sanidade: " + to_string(player->GetSP()) + "/" + to_string(PLAYER_MAX_SP);
+	GameObject* go_spdata = new GameObject();
+	CreateAddText(go_spdata, NK57, 15, sp, -1, -1, color.black, 0);
+	go_spdata->box.SetCenterPosition(graybar->box.GetCenter());
+	AddObject(go_spdata);
+}
+
+void EndState::LoadText(string id) {
+	Colors color = Colors::GetInstance();
+
+	GameObject* newText = new GameObject;
+	newText->box.SetPosition(*new Vec2(570,10));
+	newText->box.SetDimensions(650, 600);
+
+	string text;
+
+	if (id == "vitoria diabrete") {
+		text = ReadAllFromFile("text/vitoriadiabrete.txt");
+	}
+
+	if (id == "exorcizar diabrete") {
+		text = ReadAllFromFile("text/exorcizadiabrete.txt");
+	}
+
+	if (id == "pacto diabrete") {
+		text = ReadAllFromFile("text/pactodiabrete.txt");
+	}
+
+	Text* eventText = CreateAddText(newText, PETROV, REGULAR_SIZE, text, 650, 600, color.white, 0);
+
+	Component* textScroller = new ScrollerText(*newText, eventText, id);
+	newText->AddComponent(textScroller);
+
+	AddObject(newText);
+}
+
+void EndState::LoadButton(string id, string position) {
+	Colors color = Colors::GetInstance();
+
+	GameObject* button = new GameObject;
+	CreateAddButton(button, "main menu", 505, 121, *new Vec2(0, 0), id);
+
+	if (id == "deal") {
+		CreateAddText(button, NK57, 40, "Criar pacto", -1, -1, color.white, 0);
+	}
+
+	if (id == "exorcism") {
+		CreateAddText(button, NK57, 40, "Exorcizar", -1, -1, color.white, 0);
+	}
+
+	if (id == "continue") {
+		CreateAddText(button, NK57, 40, "Continuar", -1, -1, color.white, 0);
+	}
+
+	if (position == "centered") {
+		button->box.SetCenterPosition(*new Vec2(900, 760));
+	}
+	if (position == "1 of 2") {
+		button->box.SetCenterPosition(*new Vec2(900, 700));
+	}
+	if (position == "2 of 2") {
+		button->box.SetCenterPosition(*new Vec2(900, 820));
+	}
+
+	AddObject(button);
+	button_list.push_back(button);
+}
+
+Button* EndState::GetButton(string id) {
+	Button* button;
+
+	for (int i = 0; i < (int)button_list.size(); i++) {
+		button = (Button*) button_list[i]->GetComponent("Button");
+		if (button->GetID() == id) return button;;
+	}
+	return nullptr;
+}
+
+void EndState::LoadScreen() {
+	// Carrega imagem do fundo da tela
+	GameObject* screen = new GameObject;
+	CreateAddSprite(screen, "img/screens/dialoguescreen.png", 1, 0, *new Vec2(0,0), -1, -1);
+	AddObject(screen);
+}
+
+void EndState::EraseAllObjects() {
+	while (objectArray.empty() == false) {
+		objectArray.erase(objectArray.end() - 1);
+	}
+	objectArray.clear();
+}
+
+void EndState::DeleteButton(string id) {
+	if (id == "") return;
+
+	Component* cpt = nullptr;
+	for (int i = 0; i < (int)button_list.size(); i++) {
+		cpt = button_list[i]->GetComponent("Button");
+		if (cpt != nullptr && ((Button*)cpt)->GetID() == id) {
+			button_list.erase(button_list.begin() + i);
+		}
+	}
+	GameObject* go;
+	cpt = nullptr;
+	for (int i = 0; i < (int)objectArray.size(); i++) {
+		go = objectArray[i].get();
+		cpt = go->GetComponent("Button");
+		if (cpt != nullptr && ((Button*)cpt)->GetID() == id) {
+			DeleteObject(go);
+		}
+	}
+}
+
+void EndState::DeleteText(string id) {
+	if (id == "") return;
+
+	Component* cpt = nullptr;
+	GameObject* go;
+	for (int i = 0; i < (int)objectArray.size(); i++) {
+		go = objectArray[i].get();
+		cpt = go->GetComponent("ScrollerText");
+		if (cpt != nullptr && ((ScrollerText*)cpt)->GetID() == id) {
+			DeleteObject(go);
+		}
 	}
 }
 
--- Rest in Peace/EndState.h
+++ Rest in Peace/EndState.h
@@ -3,6 +3,16 @@
 
 #include "State.h"
 #include "Music.h"
+#include "Button.h"
+
+#define AVATAR_POS_X 1268
+#define AVATAR_POS_Y 21
+#define AVATAR_SIZE 146
+#define HUDBAR_W 150
+#define HUDBAR_H 25
+#define HUD_POS_Y 85
+#define HUD_Y_OFFSET 35
+#define HUD_POS_X 1430
 
 /*
  * Classe EndState
@@ -12,14 +22,26 @@
 
 class EndState : public State {
 	Music backgroundMusic;
+	vector<GameObject*> button_list;
 	string opponent;
 	string ambient;
 	bool flagInitialScreen;
 	bool flagChoice;
+	bool end;
 
 	void LoadAmbient(string type);
 	void LoadTextAndMusic();
 	void LoadOpponent(string type);
+	void LoadScreen();
+	void LoadButton(string id, string position);
+	void LoadText(string id);
+	void LoadPlayerProfile();
+
+	void EraseAllObjects();
+	void DeleteButton(string id);
+	void DeleteText(string id);
+
+	Button* GetButton(string id);
 
 public:
 	EndState(string opponent, string ambient);
